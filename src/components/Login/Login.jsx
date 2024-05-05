adf8cf68acbc8b22633066707a8998ac4efcf055
Nicolas Esteban Muñoz Sendoya <157375427+N1kxo@users.noreply.github.com>
2024-05-05 15:30:15 -0500

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { redirect, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../AuthContext/AuthContext';
 
 const Login = () => {
